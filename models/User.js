b4f898c0a63866f6fb063fe2e23b34cc696467ea
Dasti-dev <20cs3015@rgipt.ac.in>
2024-04-10 17:48:08 +0530

    Fixed Schema bugs

--- models/User.js
+++ models/User.js
@@ -5,6 +5,10 @@ class User {
   static findByUsername(username, callback) {
     db.query('SELECT * FROM users WHERE username = ?', username, callback);
   }
+  
+  static create(data, callback) {
+    db.query('INSERT INTO users SET ?', data, callback);
+  }
 }
 
 module.exports = User;
